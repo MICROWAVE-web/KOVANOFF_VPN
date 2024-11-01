f111587c89a2ccf92380abc28a078b080fbe5a6a
Alex <playalexei@yandex.ru>
2024-11-01 16:41:51 +0700

    Fix

--- main.py
+++ main.py
@@ -176,8 +176,8 @@ if __name__ == '__main__':
     setup_application(app, dp, bot=bot)
 
     # Generate SSL context
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    # context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
 
     # And finally start webserver
-    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT, ssl_context=context)
