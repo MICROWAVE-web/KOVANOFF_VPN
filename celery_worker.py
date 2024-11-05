5dd4874034d3bf18a151b1ce963f1f18f8c3a3d3
Alex <playalexei@yandex.ru>
2024-11-05 20:55:48 +0700

    Test

--- celery_worker.py
+++ celery_worker.py
@@ -4,13 +4,12 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from celery import Celery
-
-from main import API_TOKEN
+from decouple import config
 
 # Инициализация Celery
 app = Celery('tasks', broker='redis://localhost:6379/0')
 app.conf.broker_connection_retry_on_startup = True
-bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=config('API_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
 @app.task
