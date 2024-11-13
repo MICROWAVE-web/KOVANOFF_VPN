74ea0b739b040c226a9e7aadcc2ba5dedb1c48f3
Alex <playalexei@yandex.ru>
2024-11-13 08:33:57 +0700

    cancel func

--- main.py
+++ main.py
@@ -89,10 +89,11 @@ async def get_statistic(message: types.Message):
     data = load_users()
     for usr_id, user_info in data.items():
         for subscription in user_info.get("subscriptions", []):
-            res = celery_worker.cancel_subscribtion(usr_id, subscription['panel_uuid'])
-            if res:
+            res = celery_worker.cancel_subscribtion.delay(user_id, subscription['panel_uuid'])
+            output = res.get(timeout=1)
+            if output is True:
                 suc_cancel += 1
-            else:
+            elif output is False:
                 fail_cancel += 1
     text = f"""
 Отменены: {suc_cancel}
